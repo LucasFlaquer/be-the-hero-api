f9cb550d39f9721310b3544ba17e3b593541d13c
LucasFlaquer <lucas.flaquer@gmail.com>
2023-04-17 11:46:43 -0300

    create migrations

---
+++ prisma/migrations/20230417143953_create_ongs_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Ong" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "whatsapp" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "uf" TEXT NOT NULL,
+
+    CONSTRAINT "Ong_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Ong_email_key" ON "Ong"("email");
