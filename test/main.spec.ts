ef804a48617a1f461112031c2d7d66ecf029cfe8
LucasFlaquer <lucas.flaquer@gmail.com>
2023-04-17 22:52:15 -0300

    remove unused tests

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -15,15 +15,4 @@ describe('API tests', () => {
     expect(response.status).toBe(201)
     expect(response.data.id).toHaveLength(36)
   })
-  it('should be able to add an incident to an Ong', async () => {
-    const ongID = 'fc2c9b6e-53d4-47dd-9dbf-9759f3d9bf69'
-    const input = {
-      title: 'A Title',
-      description: 'A description here',
-      value: 120
-    }
-    const response = await axios.post('http://localhost:3333/incidents', input)
-    expect(response.status).toBe(201)
-    
-  })
 })
